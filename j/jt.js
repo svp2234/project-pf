8808e94f0a0cc4e522e6cfad8b2cbba84be75ee8
svp2234 <141746182+svp2234@users.noreply.github.com>
2023-11-23 12:44:14 +0530

    Add files via upload

---
+++ j/jt.js
@@ -0,0 +1,28 @@
+function myTen() {
+var n1=Number(document.getElementById('num1').value);
+var n2=Number(document.getElementById('num2').value);
+document.getElementById("de").innerHTML=n1+n2;
+}
+function mySix() {
+var n1=Number(document.getElementById('num1').value);
+var n2=Number(document.getElementById('num2').value);
+document.getElementById("en").innerHTML=n1-n2;
+}
+
+function myFix() {
+var n1=Number(document.getElementById('num1').value);
+var n2=Number(document.getElementById('num2').value);
+document.getElementById("nd").innerHTML=n1*n2;
+}
+
+function myMix() {
+var n1=Number(document.getElementById('num1').value);
+var n2=Number(document.getElementById('num2').value);
+document.getElementById("di").innerHTML=n1/n2;
+}
+
+function myNix() {
+var n1=Number(document.getElementById('num1').value);
+var n2=Number(document.getElementById('num2').value);
+document.getElementById("ap").innerHTML=n1%n2;
+}
